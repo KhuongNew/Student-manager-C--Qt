802ce2d9bd5349aacda1f301151259ad04bcf482
LTKhuong <lamthuykhuong1411@gamil.com>
2025-08-09 18:43:49 +0700

    Remove build folder from tracking

--- Phammemquanlysv/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Phammemquanlysv_FILE C:/LAM/c++/ly thuyet va bai tap/ly thuyet/ctdl_gt/DoAn/Phammemquanlysv/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Phammemquanlysv.exe)
-set(__QT_DEPLOY_TARGET_Phammemquanlysv_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Phammemquanlysv_RUNTIME_DLLS D:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
